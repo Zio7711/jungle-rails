475a2d39a58efd3a593acac32a45613635cd385e
Zio <zi.ao.tan@outlook.com>
2021-06-28 21:47:22 +0000

    Add two tests when the use's last name or email is missing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,13 +16,36 @@ RSpec.describe User, type: :model do
 
     it "should not create a user if the first_name is missing" do
       @user = User.new(
-
         last_name: 'tan',
         email: 'zio@tan.com',
         password: '123',
         password_confirmation: '123'
       )
       expect(@user).not_to be_valid
+      expect(@user.errors[:first_name]).to include("can't be blank")
+    end
+
+    it "should not create a user if the last_name is missing" do
+      @user = User.new(
+        first_name: 'tan',
+        email: 'zio@tan.com',
+        password: '123',
+        password_confirmation: '123'
+      )
+      expect(@user).not_to be_valid
+      expect(@user.errors[:last_name]).to include("can't be blank")
+    end
+
+    it "should not create a user if the email is missing" do
+      @user = User.new(
+        first_name: 'tan',
+        last_name: 'zio',
+
+        password: '123',
+        password_confirmation: '123'
+      )
+      expect(@user).not_to be_valid
+      expect(@user.errors[:email]).to include("can't be blank")
     end
 
 
