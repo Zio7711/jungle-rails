3a1ee89aabe039f34904159df15efd852345ebae
Zio <zi.ao.tan@outlook.com>
2021-06-29 19:21:43 +0000

    Add test for home page

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,17 +1,19 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
-require "capybara/rails"
-require "capybara/rspec"
-require "capybara/poltergeist"
+
 
 require File.expand_path('../config/environment', __dir__)
-Capybara.javascript_driver = :poltergeist
+
 
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+
+require "capybara/rails"
+require "capybara/rspec"
+require "capybara/poltergeist"
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -65,3 +67,5 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+
+Capybara.javascript_driver = :poltergeist
